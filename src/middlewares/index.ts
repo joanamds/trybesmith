8eac94fdeb1aca7b23da4f84360bb9abb318637f
joanamds <joanamdsantos1@gmail.com>
2023-03-15 11:28:48 -0300

    feat: requisito 7 finalizado, validações para a criação de usuários

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { UserLogin, IProduct } from '../interfaces';
+import { UserLogin, IProduct, IUser } from '../interfaces';
 
 export function validateLogin(
   req: Request,
@@ -33,7 +33,7 @@ export function validateProductName(
     return res.status(422).json({ message: '"name" must be a string' });
   }
 
-  if (name.length < 2) {
+  if (name.length < 3) {
     return res.status(422).json({ message: '"name" length must be at least 3 characters long' });
   }
 
@@ -53,10 +53,93 @@ export function validateProductAmount(
   if (typeof amount !== 'string') {
     return res.status(422).json({ message: '"amount" must be a string' });
   }
-
-  if (amount.length < 2) {
+  if (amount.length < 3) {
     return res.status(422).json({ message: '"amount" length must be at least 3 characters long' });
   }
 
   next(); 
+}
+
+export function validateUsername(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { username } = req.body as IUser;
+
+  if (!username) {
+    return res.status(400).json({ message: '"username" is required' });
+  }
+  if (typeof username !== 'string') {
+    return res.status(422).json({ message: '"username" must be a string' });
+  }
+  if (username.length < 3) {
+    return res.status(422).json({
+      message: '"username" length must be at least 3 characters long',
+    });
+  }
+  next();
+}
+
+export function validateVocation(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { vocation } = req.body as IUser;
+
+  if (!vocation) {
+    return res.status(400).json({ message: '"vocation" is required' });
+  }
+  if (typeof vocation !== 'string') {
+    return res.status(422).json({ message: '"vocation" must be a string' });
+  }
+  if (vocation.length < 3) {
+    return res.status(422).json({
+      message: '"vocation" length must be at least 3 characters long',
+    });
+  }
+  next();
+}
+
+export function validateLevel(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { level } = req.body as IUser;
+  console.log(level);
+  if (level < 1) {
+    return res.status(422).json({
+      message: '"level" must be greater than or equal to 1',
+    });
+  }
+  if (!level) {
+    return res.status(400).json({ message: '"level" is required' });
+  }
+  if (typeof level !== 'number') {
+    return res.status(422).json({ message: '"level" must be a number' });
+  }
+  next();
+}
+
+export function validatePassword(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { password } = req.body as IUser;
+
+  if (!password) {
+    return res.status(400).json({ message: '"password" is required' });
+  }
+  if (typeof password !== 'string') {
+    return res.status(422).json({ message: '"password" must be a string' });
+  }
+  if (password.length < 8) {
+    return res.status(422).json({
+      message: '"password" length must be at least 8 characters long',
+    });
+  }
+  next();
 }
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import * as userController from '../controllers/userController';
+import {
+  validateLevel, validatePassword, validateUsername, validateVocation } from '../middlewares';
 
 const userRouter = Router();
 
-userRouter.post('/', userController.create);
+userRouter.post( 
+  '/',
+  validateUsername,
+  validateVocation, 
+  validateLevel,
+  validatePassword,
+  userController.create,
+);
 userRouter.get('/', userController.getAll);
 
 export default userRouter;
